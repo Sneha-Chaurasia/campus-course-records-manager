045c78ba2bc7b14870456c7e9c5549f0d4867537
Sneha Chaurasia <snehachaurasia203@gmail.com>
2025-09-25 22:32:03 +0530

    Create run.bat

---
+++ run.bat
@@ -0,0 +1,15 @@
+@echo off
+echo ========================================
+echo      CCRM - Run Application
+echo ========================================
+echo.
+
+echo Starting application...
+echo Make sure you have compiled the project first using 'compile-and-run.bat' or manually.
+echo.
+
+java -cp . edu.ccrm.CCRMApplication
+
+echo.
+echo Application finished.
+pause
